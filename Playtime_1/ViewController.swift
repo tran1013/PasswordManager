0426aa33efb6982e881cdf3c6d28fb3499605d34
Anh Duc Tran <anhduc.t92@gmail.com>
2017-03-31 20:30:51 +0200

    Initial Commit

---
+++ Playtime_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Playtime_1
+//
+//  Created by Anh Duc Tran on 31.03.17.
+//  Copyright © 2017 Anh Duc Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
